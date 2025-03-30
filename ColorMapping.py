86911f28eb7c5ed9d181af9ebcfd7d710637ca68
LucasNebelung <lucasnebelung@gmail.com>
2025-03-30 13:07:27 +0200

    first commit

---
+++ ColorMapping.py
@@ -0,0 +1,16 @@
+###########################
+### Defining Consistent Color Mapping 
+##########################
+
+
+
+# define consistent color mapping
+color_mapping = {
+    "Physics": "Purple",
+    "Computer Science": "orange",
+    "Mathematics": "blue",
+    "Statistics": "red",
+    "Quantitative Biology": "green",
+    "Quantitative Finance": "brown",
+    "Other": "gray"
+}
